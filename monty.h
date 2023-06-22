5ca9e89a99f83c3d3ff33a2b687baa42dd1ac2a0
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-06-23 01:41:58 +0300

    Update monty.h

--- monty.h
+++ monty.h
@@ -39,10 +39,9 @@ typedef struct instruction_s
 } instruction_t;
 
 /* Function prototypes */
-void push(stack_t **stack, unsigned int line_number);
-void pall(stack_t **stack, unsigned int line_number);
-int is_number(char *str);
-void execute_opcode(char *opcode, stack_t **stack, unsigned int line_number);
-void free_stack(stack_t *stack);
-
+void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void f_add(stack_t **head, unsigned int counter);
+void f_nop(stack_t **head, unsigned int counter);
 #endif /* MONTY_H */
