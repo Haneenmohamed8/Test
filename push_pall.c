220dc3ff10e73db6de7947fc6ed82e1a9f3ddfe3
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-06-23 00:06:46 +0300

    Create push_pall.c

---
+++ push_pall.c
@@ -0,0 +1,50 @@
+#include "monty.h"
+
+/**
+ * push - Pushes an element to the stack.
+ * @stack: Double pointer to the head of the stack.
+ * @line_number: Line number where the push opcode is encountered.
+ */
+void push(stack_t **stack, unsigned int line_number)
+{
+    char *argument = strtok(NULL, " \t\n");
+
+    if (argument == NULL || !is_number(argument))
+    {
+        fprintf(stderr, "L%d: usage: push integer\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    int value = atoi(argument);
+    stack_t *new_node = malloc(sizeof(stack_t));
+    if (new_node == NULL)
+    {
+        fprintf(stderr, "Error: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    new_node->n = value;
+    new_node->prev = NULL;
+    new_node->next = *stack;
+
+    if (*stack != NULL)
+        (*stack)->prev = new_node;
+
+    *stack = new_node;
+}
+
+/**
+ * pall - Prints all the values on the stack.
+ * @stack: Double pointer to the head of the stack.
+ * @line_number: Line number where the pall opcode is encountered.
+ */
+void pall(stack_t **stack, unsigned int line_number)
+{
+    stack_t *current = *stack;
+
+    while (current != NULL)
+    {
+        printf("%d\n", current->n);
+        current = current->next;
+    }
+}
