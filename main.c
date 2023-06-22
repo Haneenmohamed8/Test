1a2c9fc08cfad037b459dfd5f42eb4e43e3a9067
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-06-23 00:16:25 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,15 +1,40 @@
 #include "monty.h"
 
+/**
+ * main - Entry point of the Monty interpreter.
+ * @argc: Number of command-line arguments.
+ * @argv: Array of command-line arguments.
+ * Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure.
+ */
 int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
-        error_usage();
-        exit(EXIT_FAILURE);
+        fprintf(stderr, "USAGE: monty file\n");
+        return EXIT_FAILURE;
     }
 
-    char *filename = argv[1];
-    interpret_file(filename);
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        return EXIT_FAILURE;
+    }
 
-    return 0;
+    stack_t *stack = NULL;
+    char *line = NULL;
+    size_t line_size = 0;
+    unsigned int line_number = 0;
+
+    while (getline(&line, &line_size, file) != -1)
+    {
+        line_number++;
+        execute_opcode(line, &stack, line_number);
+    }
+
+    free(line);
+    fclose(file);
+    free_stack(stack);
+
+    return EXIT_SUCCESS;
 }
